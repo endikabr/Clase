9b2d194d074069ca0f0c42857b9368d263fc1eb4
endikabr <endikabr@gmai.com>
2024-11-21 19:16:43 -0500

    sigo haciendo pruebas con esto

---
+++ Ficheros/censo/operacionesCenso.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#define TAMANOCENSO 100
+#define MAXTAM 50
+
+typedef struct {
+    char nombre[MAXTAM];
+    char apellidos[MAXTAM];
+    int ano; // Año de nacimiento
+    char ciudad[MAXTAM];
+} CENS;
+
+void mayoresXanos(const char *nomFich);
+
+int main(){
+
+mayoresXanos("censo.dat");
+    return 0;
+}
+
+void mayoresXanos(const char *nomFich){
+int anoEdad = 2024 - 30;
+    FILE *f = fopen(nomfich, "rb");
+    if(f == NULL){
+        printf("\nError al abrir el fichero");
+    }
+    
+    fread()
+
+fclose(f);
+}
